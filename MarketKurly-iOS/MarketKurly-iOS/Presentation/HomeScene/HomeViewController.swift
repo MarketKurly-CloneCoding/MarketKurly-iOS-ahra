107b943932f777c6484a1f323c1c21a8196df44a
ahra1221 <lizze011221@gmail.com>
2023-07-31 23:31:50 +0900

    [Feat] #3 - 필요한 ViewController 생성

---
+++ MarketKurly-iOS/MarketKurly-iOS/Presentation/HomeScene/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  MarketKurly-iOS
+//
+//  Created by 고아라 on 2023/07/31.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
