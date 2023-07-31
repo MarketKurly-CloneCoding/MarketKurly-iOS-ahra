2935d55d6e9318569f01d64b452db94c13ab0042
ahra1221 <lizze011221@gmail.com>
2023-07-31 21:54:24 +0900

    [Add] #3 - 이미지 리소스 추가

---
+++ MarketKurly-iOS/MarketKurly-iOS/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,21 @@
+//
+//  ImageLiterals.swift
+//  MarketKurly-iOS
+//
+//  Created by 고아라 on 2023/07/30.
+//
+
+import UIKit
+
+enum ImageLiterals {
+    enum TabBar {
+        static var ic_home: UIImage { .load(name: "home") }
+        static var ic_homeOn: UIImage { .load(name: "home_on") }
+        static var ic_category: UIImage { .load(name: "category") }
+        static var ic_categoryOn: UIImage { .load(name: "category_on") }
+        static var ic_search: UIImage { .load(name: "search") }
+        static var ic_searchOn: UIImage { .load(name: "search_on") }
+        static var ic_mypage: UIImage { .load(name: "mypage") }
+        static var ic_mypageOn: UIImage { .load(name: "mypage_on") }
+    }
+}
