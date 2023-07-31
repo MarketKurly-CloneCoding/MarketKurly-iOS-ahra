b070442c0d50175ddf107622c997e62200b8da38
ahra1221 <lizze011221@gmail.com>
2023-07-31 21:55:14 +0900

    [Add] #3 - 컬러 리소스 추가

---
+++ MarketKurly-iOS/MarketKurly-iOS/Global/Literals/ColorLiterals.swift
@@ -0,0 +1,27 @@
+//
+//  ColorLiterals.swift
+//  MarketKurly-iOS
+//
+//  Created by 고아라 on 2023/07/31.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var Gray400: UIColor {
+        return UIColor(hex: "#DDDDDD")
+    }
+    
+    static var PrimaryPurple: UIColor {
+        return UIColor(hex: "#5F0080")
+    }
+    
+    static var White: UIColor {
+        return UIColor(hex: "#FFFFFF")
+    }
+    
+    static var Black: UIColor {
+        return UIColor(hex: "#000000")
+    }
+}
--- MarketKurly-iOS/MarketKurly-iOS/Global/Literals/ImageLiteral.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ImageLiteral.swift
-//  MarketKurly-iOS
-//
-//  Created by 고아라 on 2023/07/30.
-//
-
-import Foundation
