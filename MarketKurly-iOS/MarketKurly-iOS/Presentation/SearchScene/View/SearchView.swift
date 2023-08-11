8f81a9ff0e0a9a7d3a59c6d59533769b8722682c
ahra1221 <lizze011221@gmail.com>
2023-08-11 23:45:43 +0900

    [Feat] #9 - 텍스트필드 구현

--- MarketKurly-iOS/MarketKurly-iOS/Presentation/SearchScene/View/SearchView.swift
+++ MarketKurly-iOS/MarketKurly-iOS/Presentation/SearchScene/View/SearchView.swift
@@ -20,6 +20,17 @@ final class SearchView: UIView {
         return view
     }()
     
+    private let searchField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "검색어를 입력해주세요"
+        textfield.layer.cornerRadius = 4
+        textfield.backgroundColor = .Gray500
+        textfield.setLeftPadding(amount: 40)
+        textfield.setPlaceholderColor(.Gray300)
+        textfield.addLeftImage(image: ImageLiterals.TabBar.ic_searchOn)
+        return textfield
+    }()
+    
     // MARK: - Life Cycles
     
     override init(frame: CGRect) {
@@ -42,12 +53,19 @@ private extension SearchView {
     }
     
     func setLayout() {
-        self.addSubview(navigationBarView)
+        self.addSubviews(navigationBarView, searchField)
         
         navigationBarView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(SizeLiterals.Screen.screenHeight * 94 / 812)
+        }
+        
+        searchField.snp.makeConstraints {
+            $0.top.equalTo(navigationBarView.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth * 343 / 375)
+            $0.height.equalTo(44)
         }
     }
 }
